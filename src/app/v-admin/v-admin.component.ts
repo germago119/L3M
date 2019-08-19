c87cfac2ed6feb82941d3c6d3d633f375dff2f41
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2019-08-19 11:16:39 -0600

    Responsive Tables

--- src/app/v-admin/v-admin.component.ts
+++ src/app/v-admin/v-admin.component.ts
@@ -7,82 +7,9 @@ import {Component, OnInit} from '@angular/core';
 })
 export class VAdminComponent implements OnInit {
 
-  roles = false;
-  sucursales = false;
-  trabajadores = false;
-  proveedores = false;
-  productos = false;
-  compras = false;
-  horas = false;
-  planilla = false;
-
   constructor() {
   }
-
-  activarHoras() {
-    this.horas = true;
-  }
-
-  desactivarHoras() {
-    this.horas = false;
-  }
-
-  activarPlanilla() {
-    this.planilla = true;
-  }
-
-  desactivarPlanilla() {
-    this.planilla = false;
-  }
-
-  activarSucursales() {
-    this.sucursales = true;
-  }
-
-  desactivarSucursales() {
-    this.sucursales = false;
-  }
-
-  activarRoles() {
-    this.roles = true;
-  }
-
-  desactivarRoles() {
-    this.roles = false;
-  }
-
-  activarTrabajadores() {
-    this.trabajadores = true;
-  }
-
-  desactivarTrabajadores() {
-    this.trabajadores = false;
-  }
-
-  activarProveedores() {
-    this.proveedores = true;
-  }
-
-  desactivarProveedores() {
-    this.proveedores = false;
-  }
-
-  activarProductos() {
-    this.productos = true;
-  }
-
-  desactivarProductos() {
-    this.productos = false;
-  }
-
-  activarCompras() {
-    this.compras = true;
-  }
-
-  desactivarCompras() {
-    this.compras = false;
-  }
-
+  
   ngOnInit() {
   }
 
